46cb8972fa69c5419c332ab101eb1eb4184a8263
UmerAhmed786 <umerahmedinfo@gmail.com>
2025-12-08 03:57:00 +0000

    container config done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 123456
+let accountEmail = "umer@gmail.com"
+var accountPassword = "252525"
+accountCity = "Karachi"
+let accountState;
+
+
+accountEmail = "ahmed@gmail.com"
+accountPassword = "3232323"
+accountCity = "Lahore"
+
+
+
+console.log(accountCity);
+
+// console.table([accountId, accountEmail, accountCity, accountPassword, accountState);
