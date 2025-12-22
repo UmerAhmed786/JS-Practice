a47d99cf6e01f030e99570be5b90212903f50059
UmerAhmed786 <umerahmedinfo@gmail.com>
2025-12-22 04:16:57 +0000

    conversionOperation

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,70 @@
+// *********************** Conversion ***********************
+
+let score = "hitesh"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "umer"
+// console.log(isLoggedIn);
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(33)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// *********************** Operations ***********************
+
+let volue = 33
+let negvolue = -volue
+// console.log(negvolue);
+
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2+2);
+// console.log(2/3);
+// console.log(2**3);
+// console.log(2%3);
+
+let str1 = "umer"
+let str2 = " ahmed"
+
+let str3 = str1 + str2
+// console.log(str3);
+// console.log(typeof str3);
+
+let num1, num2, num3
+let num1 + num2 + num3 = 2+2
+
+let gamecounter = 100
+++gamecounter
+console.log(gamecounter);
+
+
+
+
+
+
+
+
+
+
+
