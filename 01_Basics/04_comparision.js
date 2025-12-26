57ddad9e9768690f594da4df8b45ab75b7ccd0bb
UmerAhmed786 <umerahmedinfo@gmail.com>
2025-12-26 03:46:36 +0000

    comparision

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 == 1);
+// console.log(8 < 5);
+// console.log(9 > 3);
+// console.log(6 != 4);
+// console.log(2 >= 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// console.log(null == 0);
+// console.log(null > 0);
+// console.log(null >= 0);
+
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+console.log("2" === 2);
