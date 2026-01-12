30ef9ab3843bf1ea6dece5e2311c3f89a43e972c
UmerAhmed786 <umerahmedinfo@gmail.com>
2026-01-12 05:11:47 +0000

    06_nums_and_math.js

---
+++ 01_Basics/06_nums_and_math.js
@@ -0,0 +1,40 @@
+const score = 804
+// console.log(typeof score);
+
+const balance = new Number (21)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(10));
+
+const otherNumber = 123.698547
+// console.log(otherNumber.toPrecision(5));
+
+const hendreds = 100000
+// console.log(hendreds.toLocaleString('en-pa'));
+
+//=========== Maths =================================
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(5.8));
+// console.log(Math.min(5, 8, 6, 9));
+// console.log(Math.max(8, 2.1, 9, 4));
+
+console.log(Math.random());
+console.log((Math.random()*10) +1);
+console.log(Math.ceil(Math.random()*10) +1);
+
+const min = 25
+const max = 40
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
+
