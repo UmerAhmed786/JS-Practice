0fa1d1118172701230da3a6fc91f91b4fb725ded
UmerAhmed786 <umerahmedinfo@gmail.com>
2026-01-15 03:25:28 +0000

    datesinJS

---
+++ 01_Basics/07_datesinJs.js
@@ -0,0 +1,33 @@
+let myDate = new Date()
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleTimeString());
+
+
+// let myCreatedDate = new Date(2026, 0, 15)
+// let myCreatedDate = new Date(2026, 0, 15, 8, 10)
+// let myCreatedDate = new Date(15, 0, 2026)
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeZone = Date.now();
+// console.log(myTimeZone.get);
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getDate());
+// console.log(newDate.getDay());
+
+let toLocaleString('default', {
+    Weekday= "long";
+} )
+
+
+
+
+
+
+
