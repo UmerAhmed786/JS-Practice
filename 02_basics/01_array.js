0494bcb00710d43bc5e4245dcf4e123492e29aca
UmerAhmed786 <umerahmedinfo@gmail.com>
2026-01-17 04:58:07 +0000

    Array_1

---
+++ 02_basics/01_array.js
@@ -0,0 +1,38 @@
+const myArray = [0, 1, 2, 3, 4, 5]
+const myFriend = ["Rafay", "Rayyan"]
+
+const myArray2 = new Array(1, 2, 3, 4)
+// console.log(myArray);
+
+// Array Method
+
+// myArray.push(6)
+// myArray.push(7)
+// myArray.pop()
+
+// myArray.unshift(8)
+// myArray.shift()
+
+// console.log(myArray.includes(3));
+// console.log(myArray.indexOf(5));
+
+// const newArray = myArray.join()
+
+// console.log(typeof myArray);
+// console.log(typeof newArray);
+
+// slice & splice
+
+console.log("A ", myArray);
+
+const myArr1 = myArray.slice(1, 3)
+console.log(myArr1);
+console.log("B ", myArray);
+
+const myArr2 = myArray.splice(1, 3)
+console.log("C ", myArray);
+console.log(myArr2);
+
+
+
+
